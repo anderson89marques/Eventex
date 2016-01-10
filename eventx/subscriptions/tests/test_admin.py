723894cc72941430b726a49dcc32b2b7bdc47c45
anderson marques <andersonoanjo18@gmail.com>
2016-01-10 01:50:16 -0300

    Marca como paga

---
+++ eventx/subscriptions/tests/test_admin.py
@@ -0,0 +1,43 @@
+from unittest.mock import Mock
+
+from django.test import TestCase
+from eventx.subscriptions.admin import SubscriptionModelAdmin, Subscription, admin
+
+
+class SubscriptionModelTestAdmin(TestCase):
+
+    def setUp(self):
+        Subscription.objects.create(
+            name="Anderson Marques",
+            cpf="00644321229",
+            email="andersonoanjo18@gmail.com",
+            phone="984593967"
+        )
+
+        self.model_admin = SubscriptionModelAdmin(Subscription, admin.site)
+
+    def test_has_action(self):
+        """action mark_as_paid should be installed"""
+        self.assertIn('mark_as_paid', self.model_admin.actions)
+
+    def test_mark_all(self):
+        """Should mark all selected  subscription as paid"""
+        self.call_action()
+        self.assertEqual(1, Subscription.objects.filter(paid=True).count())
+
+    def test_message(self):
+        """It should send a message to the user"""
+        mock = self.call_action()
+        mock.assert_called_once_with(None, "1 inscrição foi marcada como paga.")
+
+    def call_action(self):
+        query_set = Subscription.objects.all()
+        mock = Mock()
+        old_message_user = SubscriptionModelAdmin.message_user
+        SubscriptionModelAdmin.message_user = mock
+
+        self.model_admin.mark_as_paid(None, query_set)
+
+        SubscriptionModelAdmin.message_user = old_message_user
+
+        return mock
